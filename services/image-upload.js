de4808e14a31ee3836a68c3bf35b3fd8efe76fce
Guilherme <guifelicio7@gmail.com>
2022-12-04 21:50:04 -0300

    ajustes no upload de imagem

--- services/image-upload.js
+++ services/image-upload.js
@@ -11,44 +11,42 @@ exports.uploadImage = (req, res, next) => {
   const idConsumidor = res.locals.userData?.idConsumidor
     ? res.locals.userData?.idConsumidor
     : null;
-  const idEstabelecimento = res.locals.userData?.idAdministrador
+  const idEstabelecimento = res.locals.userData?.idEstabelecimento
+    ? res.locals.userData?.idEstabelecimento
+    : null;
+  const idAdministrador = res.locals.userData?.idAdministrador
     ? res.locals.userData?.idAdministrador
     : null;
-  const idAdministrador = res.locals.userData?.idAdmin
-    ? res.locals.userData?.idAdmin
-    : null;
+
+  console.log(idConsumidor);
 
   if (idEstabelecimento) {
     Establishment.update(
       { urlImage: req.file.path },
-      { where: idEstabelecimento }
-    ).then(() => {
-      return res
-        .status(200)
-        .json({ message: "Imagem atualizada com sucesso!" });
-    });
-  } else if (idConsumidor) {
-    console.log(req.file.path);
-    Consumer.update(
-      { urlImage: req.file.path },
-      { where: { idConsumidor } }
-    ).then(() => {
-      return res
-        .status(200)
-        .json({
-          message: "Imagem Atualizada com sucesso!",
-          path: req.file.path,
-        });
-    });
-  } else if (idAdministrador) {
-    console.log(res.locals);
-
-    Admin.update({ urlImage: req.file.path }, { where: idAdministrador }).then(
-      () => {
+      { where: { idEstabelecimento } }
+    )
+      .then(() => {
         return res
           .status(200)
           .json({ message: "Imagem atualizada com sucesso!" });
-      }
-    );
+      })
+      .catch((err) => res.status(500).json({ erro: err }));
+  } else if (idConsumidor) {
+    Consumer.update({ urlImage: req.file.path }, { where: { idConsumidor } })
+      .then(() => {
+        return res.status(200).json({
+          message: "Imagem Atualizada com sucesso!",
+          path: req.file.path,
+        });
+      })
+      .catch((err) => res.status(500).json({ erro: err }));
+  } else if (idAdministrador) {
+    Admin.update({ urlImage: req.file.path }, { where: { idAdministrador } })
+      .then(() => {
+        return res
+          .status(200)
+          .json({ message: "Imagem atualizada com sucesso!" });
+      })
+      .catch((err) => res.status(500).json({ erro: err }));
   } else res.status(500).json({ message: "Erro ao atualizar Imagem" });
 };
